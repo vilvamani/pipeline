6d5f76682dc79fd5050ae27f8711d0080fdf802e
vilvamani <vilvamani007@gmail.com>
2024-11-04 16:12:55 +0530

    test

---
+++ src/test/java/testing/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package testing.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
